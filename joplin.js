aab9cada6d7ef75cc98f4ec261f43b49a0a2cf81
Ian Cooperman <ian.pl.cooperman@gmail.com>
2022-08-02 12:36:49 -0700

    Changed note retrieval URL parameters to obtain additional useful info

--- joplin.js
+++ joplin.js
@@ -8,7 +8,7 @@ url.searchParams.append("token", process.env.JOPLIN_TOKEN)
 async function getNotesFromNotebook(notebookID) {
     let foldersURL = new URL("http://localhost:" + process.env.JOPLIN_CLIPPER_PORT + "/folders/" + process.env.JOPLIN_NOTEBOOK_ID + "/notes")
     foldersURL.searchParams.append("token", process.env.JOPLIN_TOKEN)
-    foldersURL.searchParams.append("fields", "id,title,body")
+    foldersURL.searchParams.append("fields", "id,title,body,created_time,updated_time,latitude,longitude,altitude,is_todo,todo_due,todo_completed")
 
     const res = await fetch(foldersURL.href)
     const data = await res.json()
