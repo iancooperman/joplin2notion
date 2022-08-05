b3fe8075f5bbdaae1de9995146cd205a36665051
Ian Cooperman <ian.pl.cooperman@gmail.com>
2022-08-04 20:00:18 -0700

    Cleaned up index.js before continuing

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 // import variables from `.env`
-require('dotenv').config();
-const { Client } = require('@notionhq/client');
+import 'dotenv/config'
+import { Client } from '@notionhq/client'
+import Joplin from './joplin.js'
 
-const notion = new Client({auth: process.env.NOTION_KEY});
+const joplin = new Joplin(process.env.JOPLIN_TOKEN, process.env.JOPLIN_CLIPPER_PORT)
+const notion = new Client({auth: process.env.NOTION_TOKEN});
+const joplinNotebookID = process.env.JOPLIN_NOTEBOOK_ID;
+const notionDatabaseID = process.env.NOTION_DATABASE_ID;
 
-const databaseID = process.env.NOTION_DATABASE_ID;
+// async function addItem(text) {
+//     try  {
+//         const response = await notion.pages.create({
+//             parent: {database_id: databaseID},
+//             properties: {
+//                 title: {
+//                     title: [
+//                         {
+//                             "text": {
+//                                 "content": text
+//                             }
+//                         }
+//                     ]
+//                 }
+//             },
+//         })
+//         console.log(response);
+//         console.log("Success! Entry added.");
+//     } catch (error) {
+//         console.error(error.body);
+//     }
+// }
 
-async function addItem(text) {
-    try  {
-        const response = await notion.pages.create({
-            parent: {database_id: databaseID},
-            properties: {
-                title: {
-                    title: [
-                        {
-                            "text": {
-                                "content": text
-                            }
-                        }
-                    ]
-                }
-            },
-        })
-        console.log(response);
-        console.log("Success! Entry added.");
-    } catch (error) {
-        console.error(error.body);
-    }
-}
-
-addItem("Yurts in Big Sur, California");
+// addItem("Yurts in Big Sur, California");
