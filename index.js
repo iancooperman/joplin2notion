876c74ba41b489ea5e78fc902b0d3d5f57460d94
Ian Cooperman <ian.pl.cooperman@gmail.com>
2022-08-06 16:37:12 -0700

    Wrote rudimentary creation of Notion pages with title and body

--- index.js
+++ index.js
@@ -8,27 +8,66 @@ const notion = new Client({auth: process.env.NOTION_TOKEN});
 const joplinNotebookID = process.env.JOPLIN_NOTEBOOK_ID;
 const notionDatabaseID = process.env.NOTION_DATABASE_ID;
 
-// async function addItem(text) {
-//     try  {
-//         const response = await notion.pages.create({
-//             parent: {database_id: databaseID},
-//             properties: {
-//                 title: {
-//                     title: [
-//                         {
-//                             "text": {
-//                                 "content": text
-//                             }
-//                         }
-//                     ]
-//                 }
-//             },
-//         })
-//         console.log(response);
-//         console.log("Success! Entry added.");
-//     } catch (error) {
-//         console.error(error.body);
-//     }
-// }
+
+// essentially a main function
+async function processNotes() {
+    const data = await joplin.getNotesFromNotebook(joplinNotebookID, ["title", "body", "created_time", "updated_time"])
+    for (let note of data) {
+        addPageToNotionDatabase(notionDatabaseID, note.title, note.body)
+    }
+}
+
+processNotes()
+
+
+async function addPageToNotionDatabase(databaseID, title, body) {
+    try  {
+        const response = await notion.pages.create({
+            parent: {database_id: databaseID},
+            properties: {
+                title: {
+                    title: [
+                        {
+                            "type": "text",
+                            "text": {
+                              "content": title,
+                              "link": null
+                            },
+                            "annotations": {
+                              "bold": false,
+                              "italic": false,
+                              "strikethrough": false,
+                              "underline": false,
+                              "code": false,
+                              "color": "default"
+                            },
+                            "plain_text": "Grocery List",
+                            "href": null
+                          }
+                    ]
+                }
+            },
+            children: [
+                {
+                    object: "block",
+                    type: "paragraph",
+                    paragraph: {
+                        "rich_text": [
+                            {
+                                "text": {
+                                    "content": body
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        })
+        console.log(response);
+        console.log("Success! Entry added.");
+    } catch (error) {
+        console.error(error.body);
+    }
+}
 
 // addItem("Yurts in Big Sur, California");
